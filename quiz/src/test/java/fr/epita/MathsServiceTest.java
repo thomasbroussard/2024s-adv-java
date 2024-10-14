da81a16a785bb8ff5b120364a0e5908fa6fbdc06
Présentation <tbr@tbr.com>
2024-10-14 16:07:31 +0200

    included assertj commit (jee / maven / junit)

--- quiz/src/test/java/fr/epita/MathsServiceTest.java
+++ quiz/src/test/java/fr/epita/MathsServiceTest.java
@@ -1,7 +1,12 @@
 package fr.epita;
 
 import fr.epita.services.MathsServices;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+
 
 public class MathsServiceTest {
 
@@ -17,14 +22,17 @@ public class MathsServiceTest {
         System.out.println(factorial);
     }
 
+
     @Test
-    public void testNegativeValue() throws Exception {
+    public void testNegativeValue() {
+        //given
         int testNumber = -5;
+
+        //when
         Long factorial = MathsServices.factorial(testNumber);
-        if (factorial != 120){
-            throw new Exception("unexpected value returned by factorial");
-        }
-        System.out.println(factorial);
+
+        //then
+        assertThat(factorial).isEqualTo(120L);
     }
 
     @Test
