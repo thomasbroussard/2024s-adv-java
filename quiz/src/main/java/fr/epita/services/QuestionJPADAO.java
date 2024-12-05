2191557083f2f8dd029ec68ab829e11f1f3ca233
Présentation <tbr@tbr.com>
2024-12-05 10:52:39 +0100

    JPA integration

--- quiz/src/main/java/fr/epita/services/QuestionJPADAO.java
+++ quiz/src/main/java/fr/epita/services/QuestionJPADAO.java
@@ -2,6 +2,7 @@ package fr.epita.services;
 
 import fr.epita.quiz.datamodel.Question;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -16,43 +17,26 @@ public class QuestionJPADAO {
     EntityManager em;
 
     // Static initializer to ensure the table is created
-    public QuestionJPADAO(EntityManager em){
+    public QuestionJPADAO(EntityManager em) {
         this.em = em;
     }
 
     // Add a question
     @Transactional
     public void addQuestion(Question question) {
-       em.persist(question);
+        em.persist(question);
     }
 
     // Get all questions
     public List<Question> getAllQuestions() {
-        List<Question> questions = new ArrayList<>();
-        String query = "SELECT * FROM Question";
-        try (Connection connection = ds.getConnection();
-             Statement statement = connection.createStatement();
-             ResultSet resultSet = statement.executeQuery(query)) {
-            while (resultSet.next()) {
-                int id = resultSet.getInt("id");
-                String text = resultSet.getString("text");
-                questions.add(new Question(id, text));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return questions;
+        return em.createQuery("from Question", Question.class).getResultList();
+
     }
 
     // Delete a question by ID
+    @Transactional
     public void deleteQuestion(int id) {
-        String query = "DELETE FROM Question WHERE id = ?";
-        try (Connection connection = ds.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
-            preparedStatement.setInt(1, id);
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        Question question = em.find(Question.class, id);
+        em.remove(question);
     }
 }
