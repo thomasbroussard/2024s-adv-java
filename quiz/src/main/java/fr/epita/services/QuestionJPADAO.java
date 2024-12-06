0657d089d36663fe83cd297ebabe688c25bf54ac
Présentation <tbr@tbr.com>
2024-12-06 10:23:15 +0100

    using inheritance  and generics to reuse code

--- quiz/src/main/java/fr/epita/services/QuestionJPADAO.java
+++ quiz/src/main/java/fr/epita/services/QuestionJPADAO.java
@@ -7,31 +7,20 @@ import jakarta.transaction.Transactional;
 import java.util.List;
 
 
-public class QuestionJPADAO {
+public class QuestionJPADAO extends GenericJPADAO<Question, Integer> {
 
     EntityManager em;
 
     // Static initializer to ensure the table is created
     public QuestionJPADAO(EntityManager em) {
-        this.em = em;
+        super(em);
     }
 
-    // Add a question
-    @Transactional
-    public void add(Question question) {
-        em.persist(question);
-    }
 
     // Get all questions
     public List<Question> listAll() {
         return em.createQuery("from Question", Question.class).getResultList();
-
     }
 
-    // Delete a question by ID
-    @Transactional
-    public void deleteById(int id) {
-        Question question = em.find(Question.class, id);
-        em.remove(question);
-    }
 }
+
