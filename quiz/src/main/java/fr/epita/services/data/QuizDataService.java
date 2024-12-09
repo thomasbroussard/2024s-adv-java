1ca13df9aec2523718a67138e60af0bed4d9f1ff
Présentation <tbr@tbr.com>
2024-12-09 09:46:28 +0100

    implementing a data service

--- quiz/src/main/java/fr/epita/services/data/QuizDataService.java
+++ quiz/src/main/java/fr/epita/services/data/QuizDataService.java
@@ -1,8 +1,26 @@
 package fr.epita.services.data;
 
+import fr.epita.quiz.datamodel.Choice;
+import fr.epita.quiz.datamodel.Question;
+
+import java.util.List;
+
 public class QuizDataService {
 
+    QuestionJPADAO questionDAO;
+    ChoiceDAO choiceDAO;
+
     public QuizDataService(){
+
+    }
+
+    //create a text question
+    public void createQuestion(Question question){
+        questionDAO.add(question);
+    }
+
+    //create a MCQ question
+    public void createMCQQuestion(Question question, List<Choice> choiceList){
 
     }
 
